47fc502232c8bd64a3ddbb8d3018f02a77cd7265
Francisco Scala & Joe Webb <pair+franscan.imjoewebb@devbootcamp.com>
2014-07-26 10:49:06 -0500

    added bcrypt user model methods

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,21 @@ class User < ActiveRecord::Base
   has_many :surveys
   has_many :responses
   has_many :options, through: :responses
+  include BCrypt
+
+  def password
+    @password ||= Password.new(password_hash)
+  end
+
+  def password=(new_password)
+    @password = Password.create(new_password)
+    self.password_hash = @password
+  end
+
+  def login
+    user = User.find_by_email(params[:email])
+    return user.id if user.password == params[:password]
+    nil
+  end
+
 end
